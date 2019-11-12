c02b5f9a76589ac76ed39eb03b8099b06e4858b5
Билолиддин Корабойев <b.koraboyev@napm.uz>
2019-11-12 15:33:20 +0500

    first commit

---
+++ dav.py
@@ -0,0 +1,55 @@
+import requests
+import xmltodict
+import onevizion
+import json
+
+with open('settings.json', 'rb') as PFile:
+ password_data = json.loads(PFile.read().decode('utf-8'))
+
+
+
+url = password_data['URL']
+login = password_data['UserName']
+password = password_data['Password']
+
+APIURL =password_data['APIURL']
+AUTHTOKENDAV =password_data['AUTHTOKENDAV']
+URLS =password_data['URLS']
+
+
+
+
+res=requests.get(URLS, auth=(login, password))
+
+
+
+response = res.json()
+for data in response:
+
+    body = {'request_id': 'statement_doc',
+            'cadastre_number': data['DAVR_CADASTRAL_NUMBER'],
+            'cad_type': '0'}
+    requ = requests.post(APIURL, data=body,
+                             headers={'Authorization': AUTHTOKENDAV})
+
+    getdav = requ.json()
+
+    zapret = ''
+    if getdav['data']['result_code'] == 0:
+        zapret = 'Доступен'
+    else:
+        zapret = 'Запрет или арест'
+
+
+    cbu_list_request = onevizion.Trackor(trackorType='DAVREESTR', URL=url, userName=login, password=password)
+
+    cbu_list_request.update(data['TRACKOR_ID'],
+                            fields={
+                                'DAVR_INTEGRATION': 0,
+                                'DAVR_CADASTRAL_STATUS': zapret,
+                                'DAVR_OWNER': getdav['data']['subjects']['name'],
+
+
+                            }
+                            )
+
